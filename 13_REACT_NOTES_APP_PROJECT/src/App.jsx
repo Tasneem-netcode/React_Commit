44f1d95e24db490699abdd9b67538f7329c8cc4c
Tasneem-netcode <tasneem78899@gmail.com>
2026-02-07 00:43:45 +0530

    features

--- 13_REACT_NOTES_APP_PROJECT/src/App.jsx
+++ 13_REACT_NOTES_APP_PROJECT/src/App.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
 import { useState } from 'react'
+import { X } from 'lucide-react'
 const App = () => {
   const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+
+  const [task, setTask] = useState([])
 
   const submitHandler= (e) => {
-    e.preventDefault();
-    console.log("Submitted");
+    e.preventDefault();  
+    console.log(title, details);
+
+    // const taskObj = {
+    //   title: title,
+    //   details: details,
+    // }
+
+    const copyTask = [...task]
+    copyTask.push({title, details})
+    setTask(copyTask)
+    console.log(copyTask);
+
+    
+
     setTitle('')
+    setDetails('')
 
   }
 
+  const deleteNote =(index) =>{
+     const copyTask = [...task] // copy the task array
+     copyTask.splice(index, 1) // remove the element at the given index
+     setTask(copyTask) // update the state
+  }
+
   return (
-    <div className='h-screen lg:flex bg-black text-white '>
+    <div className='hide-scrollbar h-screen lg:flex bg-black text-white '>
       
       <form onSubmit={(e) => {
         submitHandler(e)
@@ -20,7 +44,7 @@ const App = () => {
         <h1 className='font-bold text-2xl '>Add Notes</h1>
         <input 
       type="text" 
-      title={title}
+      value={title}
       onChange={(e) => {
         setTitle(e.target.value)
       }}
@@ -28,22 +52,35 @@ const App = () => {
       placeholder='Enter Notes Heading' />
       <textarea
       type="text"  
+      value={details}
+      onChange={(e) => {
+        setDetails(e.target.value)
+      }}
       className='px-5 h-32 border-2 rounded w-full border-amber-200 '
       placeholder='Enter Notes Description' />
     
-      <button className='bg-white w-full rounded text-black px-5 py-2'>Add Notes</button>
+      <button className='bg-white w-full rounded text-black px-5 py-2 active:bg-amber-200'>Add Notes</button>
     
       </form>
 
-      <div className='lg:border-l-2 border-amber-200 p-10 lg:w-1/2'>
+      <div className='hide-scrollbar lg:border-l-2 border-amber-200 p-10 lg:w-1/2'>
       <h1 className='font-bold text-2xl '>Recent Notes</h1>
       
-      <div className='flex gap-5 flex-wrap mt-5 overflow-scroll h-full'>
-        <div className="h-52 w-42 rounded-2xl bg-amber-100"></div>
-        <div className="h-52 w-42 rounded-2xl bg-amber-100"></div>
-        <div className="h-52 w-42 rounded-2xl bg-amber-100"></div>
+      <div className='hide-scrollbar flex gap-5 flex-wrap items-start justify-start mt-5 overflow-scroll h-[90%]'>
+         
       
-          
+          {task.map((item, index) => {
+            return <div key={index} className="relative h-52 w-42 bg-cover py-7 px-4 rounded-2xl bg-amber-100">
+              <h2 onClick={() =>{ 
+                deleteNote(index)
+              }
+              } className='absolute top-2 right-2 text-2xl cursor-pointer text-black'><X color="#c41c1c" /></h2>
+            <div className='flex flex-col gap-2'>
+               <h3 className='font-bold text-xl leading-tight text-black'>{item.title}</h3>
+             <p className='text-black'>{item.details}</p>
+            </div>
+            </div>
+          })}
       </div>
         
       </div>
