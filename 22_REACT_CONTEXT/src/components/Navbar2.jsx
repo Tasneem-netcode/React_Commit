c0b550c99342122e0e65092c16326b433ffe13d3
Tasneem-netcode <tasneem78899@gmail.com>
2026-02-15 00:17:44 +0530

    context apis

---
+++ 22_REACT_CONTEXT/src/components/Navbar2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar2 = () => {
+  return (
+    <div className='nav2'>
+        <h4>Home</h4>
+        <h4>About</h4>
+        <h4>Products</h4>
+        <h4>Services</h4>
+    </div>
+  )
+}
+
+export default Navbar2
