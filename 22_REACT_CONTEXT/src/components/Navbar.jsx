bdebbde246d837f9698f2be6cab3273c4400ef9a
Tasneem-netcode <tasneem78899@gmail.com>
2026-02-15 18:02:16 +0530

    context api

--- 22_REACT_CONTEXT/src/components/Navbar.jsx
+++ 22_REACT_CONTEXT/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar2 from './Navbar2'
+import {useContext} from 'react'
+import { ThemeDataContext } from '../context/ThemeContext'
 
 const Navbar = () => {
+
+  const [theme] = useContext(ThemeDataContext)
+
   return (
-    <div className='nav'>
+    <div className= {theme === 'Light' ? 'light' : 'dark'}>
         <h2>Alika Chair</h2>
+        {/* {props.children[0]} */}
         <Navbar2 />
     </div>
   )
--- 22_REACT_CONTEXT/src/main.jsx
+++ 22_REACT_CONTEXT/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import ThemeContext from './context/ThemeContext'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ThemeContext>
     <App />
-  </StrictMode>,
+  </ThemeContext>
+
+  
 )
